43824820d9adfc7d12450e3aeb5d1f360485280b
Med Amine Karkni <amine.karkni@hotmail.fr>
2025-06-03 00:46:17 +0100

    gitlab ci

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -22,7 +22,7 @@ export const getAll = asyncHandler(
     const result = await productService.getAll(query);
 
     new SuccessResponsePaginate(
-      'All products returned successfully gaston',
+      'All products returned successfully',
       result.docs,
       result.meta
     ).send(res);
